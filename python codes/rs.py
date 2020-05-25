b043851e6c3bd1e0a063248f36048201acc06643
Rupesh-Darimisetti <49586865+Rupesh-Darimisetti@users.noreply.github.com>
2020-05-25 13:17:24 +0530

    hi
    hi

---
+++ python codes/rs.py
@@ -0,0 +1,8 @@
+import TKinter
+class RSRS:
+    def __init__(self,name,age,post):
+        self.name=name
+        self.age=age
+        self.post=post
+    def name():
+        c=_init__(Kishore,45,CNC Operator)
